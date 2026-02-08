e6dc1e5206452bc762141f5a3a60f3f1a799f892
Cryptoghana1 <tawfiqabdulai502@gmail.com>
2026-02-07 14:30:17 -1200

    Update admin.js

--- admin.js
+++ admin.js
@@ -27,72 +27,22 @@ function renderAdmin() {
       <h2>Rates (GHS)</h2>
 
       ${Object.keys(DATA.rates).map(c => `
-        <strong>${c}</strong>
-        <input
-          value="${DATA.rates[c].buy}"
-          onchange="DATA.rates['${c}'].buy = this.value"
-          placeholder="Buy rate"
-        />
-        <input
-          value="${DATA.rates[c].sell}"
-          onchange="DATA.rates['${c}'].sell = this.value"
-          placeholder="Sell rate"
-        />
+        <div style="margin-bottom:16px">
+          <strong>${c}</strong>
+
+          <div class="small">Buy rate</div>
+          <input
+            value="${DATA.rates[c].buy}"
+            onchange="DATA.rates['${c}'].buy = this.value"
+          />
+
+          <div class="small">Sell rate</div>
+          <input
+            value="${DATA.rates[c].sell}"
+            onchange="DATA.rates['${c}'].sell = this.value"
+          />
+        </div>
       `).join("")}
-
-      <h2>Wallet Addresses</h2>
-
-      ${Object.keys(DATA.wallets).map(c => `
-        <strong>${c}</strong>
-        <input
-          value="${DATA.wallets[c]}"
-          onchange="DATA.wallets['${c}'] = this.value"
-          placeholder="${c} wallet address"
-        />
-      `).join("")}
-
-      <h2>Payment Details</h2>
-
-      <strong>MTN MoMo</strong>
-      <input
-        value="${DATA.payments.momo.number}"
-        onchange="DATA.payments.momo.number = this.value"
-        placeholder="MoMo Number"
-      />
-      <input
-        value="${DATA.payments.momo.name}"
-        onchange="DATA.payments.momo.name = this.value"
-        placeholder="MoMo Name"
-      />
-
-      <strong>Vodafone Cash</strong>
-      <input
-        value="${DATA.payments.vodafone.number}"
-        onchange="DATA.payments.vodafone.number = this.value"
-        placeholder="Vodafone Number"
-      />
-      <input
-        value="${DATA.payments.vodafone.name}"
-        onchange="DATA.payments.vodafone.name = this.value"
-        placeholder="Vodafone Name"
-      />
-
-      <strong>Bank</strong>
-      <input
-        value="${DATA.payments.bank.bank}"
-        onchange="DATA.payments.bank.bank = this.value"
-        placeholder="Bank Name"
-      />
-      <input
-        value="${DATA.payments.bank.name}"
-        onchange="DATA.payments.bank.name = this.value"
-        placeholder="Account Name"
-      />
-      <input
-        value="${DATA.payments.bank.number}"
-        onchange="DATA.payments.bank.number = this.value"
-        placeholder="Account Number"
-      />
 
       <button onclick="save()">Save</button>
     </div>
