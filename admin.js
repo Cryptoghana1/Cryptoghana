79a47ef05b1f407c904c53bcb3138e31accd74d4
Cryptoghana1 <tawfiqabdulai502@gmail.com>
2026-02-07 14:35:02 -1200

    Update admin.js

--- admin.js
+++ admin.js
@@ -24,30 +24,65 @@ function login() {
 function renderAdmin() {
   admin.innerHTML = `
     <div class="card">
-      <h2>Rates (GHS)</h2>
 
+      <h2>Rates (GHS)</h2>
       ${Object.keys(DATA.rates).map(c => `
         <div style="margin-bottom:16px">
           <strong>${c}</strong>
 
           <div class="small">Buy rate</div>
-          <input
-            value="${DATA.rates[c].buy}"
-            onchange="DATA.rates['${c}'].buy = this.value"
-          />
+          <input value="${DATA.rates[c].buy}"
+            onchange="DATA.rates['${c}'].buy=this.value" />
 
           <div class="small">Sell rate</div>
-          <input
-            value="${DATA.rates[c].sell}"
-            onchange="DATA.rates['${c}'].sell = this.value"
-          />
+          <input value="${DATA.rates[c].sell}"
+            onchange="DATA.rates['${c}'].sell=this.value" />
         </div>
       `).join("")}
+
+      <h2>Wallet Addresses</h2>
+      ${Object.keys(DATA.wallets).map(c => `
+        <div style="margin-bottom:12px">
+          <strong>${c}</strong>
+          <input value="${DATA.wallets[c]}"
+            onchange="DATA.wallets['${c}']=this.value" />
+        </div>
+      `).join("")}
+
+      <h2>Payment Details</h2>
+
+      <strong>MTN MoMo</strong>
+      <input placeholder="Number"
+        value="${DATA.payments.momo.number}"
+        onchange="DATA.payments.momo.number=this.value" />
+      <input placeholder="Name"
+        value="${DATA.payments.momo.name}"
+        onchange="DATA.payments.momo.name=this.value" />
+
+      <strong>Vodafone Cash</strong>
+      <input placeholder="Number"
+        value="${DATA.payments.vodafone.number}"
+        onchange="DATA.payments.vodafone.number=this.value" />
+      <input placeholder="Name"
+        value="${DATA.payments.vodafone.name}"
+        onchange="DATA.payments.vodafone.name=this.value" />
+
+      <strong>Bank</strong>
+      <input placeholder="Bank name"
+        value="${DATA.payments.bank.bank}"
+        onchange="DATA.payments.bank.bank=this.value" />
+      <input placeholder="Account name"
+        value="${DATA.payments.bank.name}"
+        onchange="DATA.payments.bank.name=this.value" />
+      <input placeholder="Account number"
+        value="${DATA.payments.bank.number}"
+        onchange="DATA.payments.bank.number=this.value" />
 
       <button onclick="save()">Save</button>
     </div>
   `;
 }
+
 
 function save() {
   saveData(DATA);
